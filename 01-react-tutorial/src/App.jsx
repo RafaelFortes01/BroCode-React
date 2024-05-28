6cf733cdb243a68b5bce9de3d6f38197a5216406
Rafael <rafael06092001@gmail.com>
2024-05-28 00:35:48 -0300

    React Tutorial

---
+++ 01-react-tutorial/src/App.jsx
@@ -0,0 +1,17 @@
+import { Fragment } from 'react'
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Food from './Food.jsx'
+
+
+function App() {
+  return(
+    <Fragment>
+      <Header/>
+      <Footer/>
+      <Food />
+    </Fragment>
+)
+}
+
+export default App
