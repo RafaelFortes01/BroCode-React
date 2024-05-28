eed810a44ac2dcb659bfa36d52ad6de723609732
Rafael <rafael06092001@gmail.com>
2024-05-28 03:10:11 -0300

    Render List

---
+++ 06-render-list/src/List.jsx
@@ -0,0 +1,42 @@
+import propTypes from 'prop-types'
+
+function List(props) {
+
+   // fruits.sort((a, b) => a.name.localeCompare(b.name)) // ALPHABETICAL
+   // fruits.sort((a, b) => b.name.localeCompare(a.name)) // REVERSE ALPHABETICAL
+   // fruits.sort((a, b) => a.calories - b.calories) // NUMERIC
+   // fruits.sort((a, b) => b.calories - a.calories) // REVERSE NUMERIC
+
+   // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100)
+   // const highalFruits = fruits.filter(fruit => fruit.calories >= 100)
+
+   const category = props.category
+   const itemList = props.items
+
+   const listItems = itemList.map(item => <li key={item.id}>
+                                          {item.name}: &nbsp;
+                                          <b>{item.calories}</b>
+                                          </li>)
+   return(
+      <div>
+         <h3>{category}</h3>
+         <ul>
+            {listItems}
+         </ul>
+      </div>
+   )
+}
+
+List.propTypes = {
+   category: PropTypes.string,
+   items: PropTypes.arrayOf(PropTypes.shape({id: PropTypes.number,
+                                             name: PropTypes.string,
+                                             calories: PropTypes.number}))
+}
+
+List.defaultProps = {
+   category: 'Category',
+   items: []
+}
+
+export default List
