47aa6825d85d161c9e6c60d3a5b04fb1e4ccb07c
Rafael <rafael06092001@gmail.com>
2024-05-29 05:30:01 -0300

    Color Picker App

---
+++ 10-color-picker-app/src/App.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react"
+import ColorPicker from "./ColorPicker.jsx"
+function App() {
+  return (
+    <Fragment>
+      <ColorPicker />
+    </Fragment>
+    )
+}
+
+export default App
